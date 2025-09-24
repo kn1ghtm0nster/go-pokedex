dfeacee85a0fedf97b48092810f70238e5d7190b
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-09-23 20:28:45 -0400

    feat: add starting help and exit command logic

--- main.go
+++ main.go
@@ -7,8 +7,27 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main () {
 	inputScanner := bufio.NewScanner(os.Stdin)
+
+	supportedCommands := map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -27,8 +46,13 @@ func main () {
 		}
 
 		command := cleanedInput[0]
+
+		if cmd, exists := supportedCommands[command]; exists {
+			cmd.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
 		
-		fmt.Printf("Your command was: %s\n", command)
 	}
 }
 
@@ -47,4 +71,29 @@ func cleanInput(text string) []string {
 	}
 	// return the cleaned slice of words
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	commands := []string{"exit", "help"}
+	descriptions := []string{
+		"Exit the Pokedex",
+		"Displays a help message",
+	}
+
+	for i, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd, descriptions[i])
+	}
+
+	return nil
 }
